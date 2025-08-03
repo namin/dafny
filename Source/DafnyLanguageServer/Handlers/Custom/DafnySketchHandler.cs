d724f874e0597615c2426d176b8057a12d7556a3
Nada Amin <namin@seas.harvard.edu>
2025-08-03 19:27:07 -0400

    simplified how method is found from position, supporting methods inside modules as requested by @akravc

--- Source/DafnyLanguageServer/Handlers/Custom/DafnySketchHandler.cs
+++ Source/DafnyLanguageServer/Handlers/Custom/DafnySketchHandler.cs
@@ -63,22 +63,11 @@ namespace Microsoft.Dafny.LanguageServer.Handlers.Custom {
       return new SketchResponse("\n// Error: no proof sketch generated" + errorMsg + "\n"); 
     }
     private Method GetMethodFromPosition(Program resolvedProgram, Position position) {
-      //Log("# Getting Method");
       if (resolvedProgram.DefaultModuleDef is DefaultModuleDefinition defaultModule) {
-        foreach (var topLevelDecl in defaultModule.TopLevelDecls) {
-          if (topLevelDecl is TopLevelDeclWithMembers classDecl) {
-            foreach (var member in classDecl.Members) {
-              var method = member as Method;
-              if (method != null) {
-                //var methodDetails = $"lines {method.Tok.line}-{method.EndToken.line}";
-                if (IsPositionInRange(method.StartToken, method.EndToken, position)) {
-                  //Log("## Found method: " + methodDetails);
-                  return method;
-                } else {
-                  //Log("## Method out of range: " + methodDetails);
-                }
-              }
-            }
+        foreach (var (member, _) in defaultModule.AccessibleMembers) {
+          var method = member as Method;
+          if (method != null && IsPositionInRange(method.StartToken, method.EndToken, position)) {
+            return method;
           }
         }
       }
